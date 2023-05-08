e6d4a70c6f81a0b7c6e8d9f5af01786911bf1cc5
MSR-PRIV <92868824+MSR-XDC@users.noreply.github.com>
2023-05-08 11:29:38 +0200

    Tilrettet namespace i plant class

--- AnimalAbstraction/Models/SuperClasses/Plant.cs
+++ AnimalAbstraction/Models/SuperClasses/Plant.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AnimalAbstraction.Models.SuperClasses;
 
-namespace AnimalAbstraction.Models.SuperClasses
+namespace AnimalAbstraction.Models.Classes
 {
     internal class Plant : LifeForm
     {
--- AnimalAbstraction/Program.cs
+++ AnimalAbstraction/Program.cs
@@ -1,5 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using AnimalAbstraction.Models.SuperClasses;
 using AnimalAbstraction.Models.Classes;
 
 Console.WriteLine("Hello, World!");
