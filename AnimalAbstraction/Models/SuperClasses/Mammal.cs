169f0c51384dc22f2cc778b18c019ec4c926a9e9
Janni <barstadjmj@gmail.com>
2023-05-08 11:34:46 +0200

    janni

---
+++ AnimalAbstraction/Models/SuperClasses/Mammal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnimalAbstraction.Models.SuperClasses
+{
+    internal class Mammal : Animal
+    {
+
+        protected private bool hasMilk = true;
+        public Mammal(bool hasMilk, byte gender, int age) : base(gender, age)
+        {
+            this.hasMilk = hasMilk;
+        }
+    }
+}
